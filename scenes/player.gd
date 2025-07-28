9564467e3b7d066dba9dd1aed73bfa5754556f0f
dfcut8 <dfcut8@proton.me>
2025-07-28 09:41:42 -0400

    simple statements

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,1 +1,11 @@
 extends RigidBody2D
+
+func _ready() -> void:
+	apply_impulse(Vector2(25, 0))
+
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	var r = randf_range(-100, 100)
+	apply_force(Vector2(r, r))
