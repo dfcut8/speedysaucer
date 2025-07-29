ead4e0b4878c45132f6595b40bbc3b857d093d7f
dfcut8 <dfcut8@proton.me>
2025-07-28 22:12:36 -0400

    signals

--- scenes/level.gd
+++ scenes/level.gd
@@ -2,3 +2,11 @@ extends Node2D
 
 func _ready() -> void:
 	pass
+
+
+func _on_maze_body_exited(body: Node2D) -> void:
+	print("body out")
+
+
+func _on_maze_body_entered(body: Node2D) -> void:
+	print("body in")
