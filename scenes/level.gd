c9ebdc3d778ec51e954688a04b651543c0d2f817
dfcut8 <dfcut8@proton.me>
2025-07-29 10:02:09 -0400

    reload on exit

--- scenes/level.gd
+++ scenes/level.gd
@@ -6,3 +6,7 @@ func _ready() -> void:
 	
 func _body_out(body: Node2D) -> void:
 	print("maze: body_entered #1")
+
+
+func _on_maze_body_exited(body: Node2D) -> void:
+	get_tree().reload_current_scene()
